828680fd0e5462a48b986a0453941c0dd1a337c9
Smith-appwrk <smith.g@appwrk.com>
2025-05-30 18:00:56 +0530

    chore: update devDependencies in package.json
    - Added 'build' version 0.1.4
    - Added 'run' version 1.5.0
    - Added 'typescript' version 5.8.3
    - Added 'typescriptnpm' version 1.0.1

--- build.js
+++ build.js
@@ -3,7 +3,9 @@ esbuild.build({
     entryPoints: ['server/index.js'],
     bundle: true,
     platform: 'node',
-    outfile: 'dist/index.js'
+    outfile: 'dist/index.js',
+    external: ['typescript'], // Mark typescript as external
+    loader: { '.ts': 'js' }, // Handle TypeScript files
 })
     .then((r) => {
         console.log(`Build succeeded.`);
